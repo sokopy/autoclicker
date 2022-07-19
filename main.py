95a37cb4bf34b665af64fa16b2295b7ca8c5c5b9
Beriekhoi <65053744+NoneType12@users.noreply.github.com>
2022-07-19 14:11:00 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+os.system("cls")
+import pyautogui, time, pyttsx3
+try:
+    reps = int(input("How many times do you want to click? "))
+except:
+    print("The entered value is not a number. Please execute the program again.")
+    exit()
+else:
+    e = pyttsx3.Engine()
+    print("Starting clicks in: ")
+    i=5
+    while i>=1:
+        time.sleep(0.05)
+        e.say(str(i))
+        print(i)
+        e.runAndWait()
+        i-=1
+
+    for i in range(reps):
+        pyautogui.click()
